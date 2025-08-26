feabb1927f5e546f9c1f6d7bb380cf7320323ed6
SHARK YOU <41719224+sha2kyou@users.noreply.github.com>
2025-08-26 13:53:32 +0800

    Update side-calendar.rb

--- Casks/side-calendar.rb
+++ Casks/side-calendar.rb
@@ -2,7 +2,7 @@ cask "side-calendar" do
   version "0.2025.55_1"
   sha256 "07950e2d1c4bd879af7c83ec304a8f44de03ffdd1c2016e07de8eb1852abbc7f"
 
-  url "https://github.com/sha2kyou/SideCalendar/releases/download/#{version}/SideCalendar%20Lite.dmg"
+  url "https://github.com/sha2kyou/SideCalendar/releases/download/#{version}/SideCalendar.dmg"
   name "SideCalendarLite"
   desc "SideCalendar is a macOS menu bar app that unifies events and information from various sources (Calendar, Reminders, RSS, GitHub, etc.) into a dynamic screen edge information stream."
   homepage "https://github.com/sha2kyou/SideCalendar"
